465adc8020d634bb297653e074c34db899dd744b
rxian <rxian@users.noreply.github.com>
2025-04-10 01:48:26 -0500

    add jigsaw dataset and support for overlapping groups

--- linearpost/dataset.py
+++ linearpost/dataset.py
@@ -23,6 +23,12 @@ class Categorical(Feature):
 
 
 @dataclass(kw_only=True)
+class Multilabel(Feature):
+  n_labels: Optional[int] = None
+  label_names: Optional[list[str]] = None
+
+
+@dataclass(kw_only=True)
 class Array(Feature):
   column_names: Optional[list[str]] = None
   category_names: Optional[list[str]] = None
@@ -223,6 +229,8 @@ class Dataset:
     for column_name, feature in self.features.items():
       if isinstance(feature, Categorical):
         s += f'  - {column_name} ({feature.n_categories or "?"} categories)\n'
+      elif isinstance(feature, Multilabel):
+        s += f'  - {column_name} ({feature.n_labels or "?"} multilabel)\n'
       else:
         if isinstance(self.data[column_name], pd.DataFrame):
           s += f'  - {column_name} (DataFrame), shape: {self.data[column_name].shape}\n'
